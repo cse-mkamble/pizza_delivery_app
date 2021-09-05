ce48f71bdf1d23c64f80fa59c18cf88353a318af
cse-mkamble <cse.mkamble@gmail.com>
2021-09-05 14:05:19 +0530

    Filter Component Logic

--- client/src/redux/actions/pizzaActions.js
+++ client/src/redux/actions/pizzaActions.js
@@ -10,3 +10,26 @@ export const getAllPizzas = () => async dispatch => {
         dispatch({ type: constants.GET_PIZZAS_FAILED, payload: error })
     }
 }
+
+export const filterPizzas = (searchKey, category) => async dispatch => {
+    var filterPizzas
+    dispatch({ type: constants.GET_PIZZAS_REQUEST })
+    try {
+        const res = await axios.get(`/api/pizzas/getallpizzas`)
+        if ( searchKey && (category!='all')) {
+            filterPizzas = res.data.filter(pizza => pizza.name.toLowerCase().includes(searchKey))
+            filterPizzas = filterPizzas.filter(pizza => pizza.category.toLowerCase()==category)
+        } else if (category!='all') {
+            filterPizzas = res.data.filter(pizza => pizza.category.toLowerCase()==category)
+        } else {
+            filterPizzas = res.data.filter(pizza => pizza.name.toLowerCase().includes(searchKey))
+        }
+        // filterPizzas = res.data.filter(pizza => pizza.name.toLowerCase().includes(searchKey))
+        // if (category!='all') {
+        //     filterPizzas = res.data.filter(pizza => pizza.category.toLowerCase()==category)
+        // }
+        dispatch({ type: constants.GET_PIZZAS_SUCCESS, payload: filterPizzas })
+    } catch (error) {
+        dispatch({ type: constants.GET_PIZZAS_FAILED, payload: error })
+    }
+}
