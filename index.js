e98df9c6547693855f3c27f0fdd4ee860b9166fa
cse-mkamble <cse.mkamble@gmail.com>
2021-09-10 18:23:59 +0530

    client deploy herokuapp

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ app.use("/api", pizzasRoute);
 app.use("/api", userRoute)
 app.use("/api", orderRoute)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+
 // Run Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
